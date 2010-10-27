633fd1a4b0fdc4d0a4c2c56308392e64fb3ccfdb
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-10-27 17:32:38 +0900

    generated rails 3.0 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranguba::Application.config.secret_token = '3e2d85c015e209bd43ee233bbcda6eed7deda7a4ae58873f1a7a283dcdbb4566d2af1ea3dc328b16510d818791e59bb104ed16583e8df98577e9ca863ec830c2'
