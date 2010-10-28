ee0b250295d865429286d09a0e612ea9e7c3a201
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-10-28 11:51:10 +0900

    define path for the database in configurations of the rails application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ Ranguba::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.index_db_path = "/var/db/ranguba"
 end
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Ranguba::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.index_db_path = "#{RAILS_ROOT}/test/fixture/index_db"
 end
