06803cd8d72f10e933e7e8947989b31598078dc2
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-10-29 12:43:09 +0900

    redirect to "search/.../.../" style URL from "search/?..."

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
 
   def index
+    unless params[:search_request].is_a?(String)
+      path = SearchRequest.new(params[:search_request]).to_s
+      redirect_to "/search/#{path}"
+      return
+    end
+
     @search_request = SearchRequest.new
     @search_request.parse(params[:search_request])
   end
