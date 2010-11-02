e1d377a9a79332dbf70c7af362ab844c2863d206
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-02 18:25:49 +0900

    make content header and footer customizable

--- lib/ranguba/customize.rb
+++ lib/ranguba/customize.rb
@@ -3,8 +3,19 @@ module Ranguba
     class << self
       def load
         @@title = File.read("#{base}/title.txt").strip
+        @@content_header = File.read("#{base}/content_header.txt")
+        @@content_footer = File.read("#{base}/content_footer.txt")
+
         @@categories = read_key_value_list("#{base}/categories.txt")
         @@types = read_key_value_list("#{base}/types.txt")
+      end
+
+      def content_header
+        @@content_header
+      end
+
+      def content_footer
+        @@content_footer
       end
 
       def category(key)
