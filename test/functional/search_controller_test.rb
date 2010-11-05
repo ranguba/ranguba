9b083ce9502175182fbd58fb8372881475b5e5b2
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-05 18:12:13 +0900

    more tests for search controller

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
@@ -16,7 +17,7 @@ class SearchControllerTest < ActionController::TestCase
     assert_template "search/index"
   end
 
-  def test_search_request_hash
+  def test_request_hash
     post :index,
          :search_request => {:query => "q",
                              :type => "t"},
@@ -26,11 +27,83 @@ class SearchControllerTest < ActionController::TestCase
                          :page => 2
   end
 
-  def test_search_request_string
+  def test_request_hash_drilldown
+    post :index,
+         :search_request => {:type => "t",
+                             :query => "q"}
+    assert_redirected_to :action => "index",
+                         :search_request => "type/t/query/q"
+  end
+
+  def test_request_string
     post :index,
          :search_request => "query/q/type/t"
     assert_response :success
     assert_template "search/index"
+  end
+
+  def test_unknown_parameter
+    post :index,
+         :search_request => "query/q/unknown/value"
+    assert_response 400
+    assert_template "search/bad_request"
+  end
+
+  def test_parameter_with_slash
+    post :index,
+         :search_request => "query/text%2Fhtml/type/html"
+    assert_response :success
+    assert_template "search/index"
+  end
+
+  def test_encoded_parameter
+    encoded = URI.encode("日本語")
+    post :index,
+         :search_request => "query/#{encoded}/type/html"
+    assert_response :success
+    assert_template "search/index"
+  end
+
+  def test_first_page
+    post :index,
+         :search_request => "query/q",
+         :page => 1
+    assert_response :success
+    assert_template "search/index"
+  end
+
+  def test_too_large_page
+    post :index,
+         :search_request => "query/q",
+         :page => 2
+    assert_response 404
+    assert_template "search/not_found"
+
+    post :index,
+         :search_request => "query/q",
+         :page => 9999
+    assert_response 404
+    assert_template "search/not_found"
+  end
+
+  def test_too_small_page
+    post :index,
+         :search_request => "query/q",
+         :page => 0
+    assert_response 404
+    assert_template "search/not_found"
+
+    post :index,
+         :search_request => "query/q",
+         :page => -1
+    assert_response 404
+    assert_template "search/not_found"
+
+    post :index,
+         :search_request => "query/q",
+         :page => -9999
+    assert_response 404
+    assert_template "search/not_found"
   end
 
 end
