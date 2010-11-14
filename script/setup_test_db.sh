b8036caa966a73ee7a5f6ab01e5756213fc0881f
Kouhei Sutou <kou@clear-code.com>
2010-11-14 17:18:51 +0900

    remove needless scripts.

--- script/setup_test_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf tmp/database
-mkdir tmp/database
-cat test/fixtures/test_db.dump | groonga -n tmp/database/db
-
--- script/update_development_db_dump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-groonga db/groonga/db dump > test/fixtures/development_db.dump
-
