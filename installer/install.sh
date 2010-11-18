d65a4a8b0d2b83ad3e06af7db121a6f568c98d42
Kenji Okimoto <okimoto@clear-code.com>
2010-11-18 15:52:07 +0900

    split install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -29,6 +29,7 @@ until test $# = 0; do
       (--prefix)
 	shift
 	PREFIX="$1"
+	;;
       (--)
 	shift
 	break
@@ -90,22 +91,10 @@ packages=(
     curl-devel
 )
 
-function abort() {
-    local status=$?
-    if test $# = 0; then
-	echo Failed
-    else
-	echo "$*"
-    fi
-    if test $status = 0; then
-	status=1
-    fi
-    exit $status
-}
-
 function checkroot() {
     if test `id -u` -ne 0; then
-	abort "This installer must be run with administrator privileges. Aborting"
+	echo "This installer must be run with administrator privileges. Aborting"
+	exit 1
     fi
 }
 
@@ -115,7 +104,7 @@ function prepare_user() {
     fi
 }
 
-function check_packages() {
+function check_rpm_packages() {
     local missing=()
     for pkg in "${packages[@]}"; do
 	if ! msg=$(rpm -q "$pkg" 2>&1); then
@@ -128,137 +117,6 @@ function check_packages() {
 	    yum install -y $pkg 1>&$log 2>&1
 	done
     fi
-}
-
-function download() {
-    for url do
-	test -f "${SOURCE}/${url##*/}" || missing=("${missing[@]}" "$url")
-    done
-}
-
-function inst() {
-    local base="${1##*/}"
-    download "$1"
-    shift
-    installs=("${installs[@]}" "$SEPARATOR" "$base" "$@")
-}
-
-function do_install1() {
-    local base patched
-
-    echo "Install: [$(date +%Y/%m/%d-%H:%M:%S)] $1" 1>&$log
-
-    case "$1" in
-      (*.tar.bz2)
-	echo -n "Extracting $1..."
-	mkdir -p build
-	tar xpjf "${SOURCE}/$1" -C build 1>&$log 2>&1 || abort
-	echo done
-	base=${1%.tar.bz2}
-	;;
-      (*.tar.gz)
-	echo -n "Extracting $1..."
-	mkdir -p build
-	tar xpzf "${SOURCE}/$1" -C build 1>&$log 2>&1 || abort
-	echo done
-	base=${1%.tar.gz}
-	;;
-      (*.gem)
-	echo -n "Installing $1..."
-	ruby -C "${SOURCE}" -S gem install --no-ri --no-rdoc --local "$1" 1>&$log 2>&1 || abort
-	echo done
-	return
-	;;
-    esac
-
-    while shift; do
-	case "$1" in
-	  (--patch=*)
-	    echo -n "Applying patch ${1#*=} to $base..."
-	    patch -d "build/$base" -p1 < "${SOURCE}/${1#*=}" 1>&$log 2>&1 || abort
-	    patched=yes
-	    echo done
-	    ;;
-	  (*)
-	    break
-	    ;;
-	esac
-    done
-
-    (cd "build/$base"
-    if test \( -f configure.in -a ! configure -nt configure.in \) \
-	 -o \( -f configure.ac -a ! configure -nt configure.ac \); then
-	exec autoconf
-    fi) 1>&$log 2>&1
-
-    if test -f "build/$base/configure"; then
-	echo -n "Configuring $base..."
-	(
-	    cd "build/$base"
-	    exec ./configure --enable-shared --prefix="$PREFIX" "$@"
-	) 1>&$log 2>&1 || abort
-	echo done
-    fi
-
-    echo -n "Building $base..."
-    if test -f "build/$base/Makefile"; then
-	test "$patched" = yes && make -C "build/$base" prereq 1>&$log 2>&1 || true
-	make -C "build/$base" 1>&$log 2>&1 || abort
-    elif test -f "build/$base/Rakefile"; then
-	ruby -C "build/$base" -S rake 1>&$log 2>&1 || abort
-    fi
-    echo done
-
-    echo -n "Installing $base..."
-    if test -f "build/$base/GNUmakefile" -o -f "build/$base/Makefile"; then
-	make -C "build/$base" prefix="$PREFIX" install 1>&$log 2>&1 || abort
-    elif test -f "build/$base/Rakefile"; then
-	ruby -C "build/$base" -S rake install 1>&$log 2>&1 || abort
-    fi
-    echo done
-
-    rm -fr "build/$base" > /dev/null 2>&1 || :
-    rmdir build >/dev/null 2>&1 || :
-}
-
-function install_passenger() {
-    $PREFIX/bin/passenger-install-apache2-module -a 1>&$log 2>&1 || abort
-    if [ ! -f passenger.conf ]; then
-        $PREFIX/bin/passenger-install-apache2-module --snippet > passenger.conf
-    fi
-    if [ ! -f ranguba.conf ]; then
-        cat > ranguba.conf <<EOF
-<VirtualHost *:80>
-   ServerName www.example.com
-   DocumentRoot ${PREFIX}/srv/www/ranguba/public
-   <Directory ${PREFIX}/srv/www/ranguba/public>
-      AllowOverride all
-      Options -MultiViews
-   </Directory>
-</VirtualHost>
-EOF
-    fi
-}
-
-function download_all() {
-    if test ${#missing[@]} -gt 0; then
-	wget -N -P "${SOURCE}" "${missing[@]}" || abort
-    fi
-}
-
-function install_all() {
-    local args arg
-    until test ${#installs[@]} = 0; do
-	args=()
-	until test ${#installs[@]} = 0 || {
-		arg="${installs}" installs=("${installs[@]:1}")
-		test "$arg" = "$SEPARATOR"
-	    } do
-	    args=("${args[@]}" "${arg}")
-	done
-	test ${#args[@]} -gt 0 && do_install1 "${args[@]}"
-    done
-    install_passenger
 }
 
 case $ARCH in
@@ -277,117 +135,34 @@ else
     log=
 fi
 test $log && echo "Start: $(LC_ALL=C date)" 1>&$log
+test $log && chmod 0666 install.log
 
 checkroot
 prepare_user
 
 if ! test -d "${SOURCE}"; then
-    mkdir -p sources
+    sudo -u $RANGUBA_USERNAME mkdir -p sources
     SOURCE=sources
 fi
 
 if test "$nocheck" != yes; then
-    check_packages
+    check_rpm_packages
 fi
 
-export PATH="$PREFIX/bin:$PATH"
-export LD_RUN_PATH="$PREFIX/lib:${LD_RUN_PATH-/usr/$lib}"
-export LD_LIBRARY_PATH="$PREFIX/lib:${LD_LIBRARY_PATH-/usr/$lib}"
-export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:${PKG_CONFIG_PATH-/usr/lib/pkgconfig}"
-
-missing=()
-
-inst http://www.ring.gr.jp/archives/GNU/autoconf/autoconf-2.68.tar.bz2
-inst http://www.ring.gr.jp/archives/GNU/gettext/gettext-0.18.1.1.tar.gz
-inst http://www.ring.gr.jp/archives/X/gnome/sources/glib/2.24/glib-2.24.2.tar.bz2
-inst http://www.ring.gr.jp/archives/X/gnome/sources/libgsf/1.14/libgsf-1.14.19.tar.bz2
-inst ftp://xmlsoft.org/libxml2/libxml2-2.7.7.tar.gz
-inst ftp://xmlsoft.org/libxml2/libxslt-1.1.26.tar.gz
-inst http://www.ring.gr.jp/archives/X/gnome/sources/atk/1.29/atk-1.29.4.tar.bz2
-inst http://fontconfig.org/release/fontconfig-2.8.0.tar.gz
-inst http://downloads.sourceforge.net/project/freetype/freetype2/2.4.3/freetype-2.4.3.tar.bz2
-
-inst http://cairographics.org/releases/pixman-0.18.4.tar.gz
-inst http://cairographics.org/releases/cairo-1.8.10.tar.gz
-inst http://www.ring.gr.jp/archives/X/gnome/sources/pango/1.28/pango-1.28.3.tar.bz2
-
-inst http://www.ring.gr.jp/archives/X/gnome/sources/gtk+/2.20/gtk+-2.20.1.tar.bz2
-inst http://www.ring.gr.jp/archives/X/gnome/sources/libglade/2.6/libglade-2.6.4.tar.bz2
-inst http://poppler.freedesktop.org/poppler-0.14.4.tar.gz
-inst http://poppler.freedesktop.org/poppler-data-0.4.3.tar.gz
-
-inst http://downloads.sourceforge.net/project/wvware/wv/1.2.4/wv-1.2.4.tar.gz
-inst http://www.ring.gr.jp/archives/X/gnome/sources/goffice/0.8/goffice-0.8.11.tar.bz2
-inst http://www.ring.gr.jp/archives/X/gnome/sources/gnumeric/1.10/gnumeric-1.10.11.tar.gz
-inst http://www.ring.gr.jp/archives/X/gnome/sources/gnumeric/1.10/gnumeric-1.10.11.tar.gz
-inst http://sourceforge.net/projects/asciidoc/files/asciidoc/8.6.2/asciidoc-8.6.2.tar.gz
-inst http://dag.wieers.com/home-made/unoconv/unoconv-0.4.tar.bz2
-inst http://ftp.kddilabs.jp/infosystems/apache//httpd/httpd-2.2.17.tar.bz2
-
-inst http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p0.tar.bz2 \
-     --disable-install-doc
-inst http://rubygems.org/downloads/pkg-config-1.0.7.gem
-inst http://downloads.sourceforge.net/project/ruby-gnome2/ruby-gnome2/ruby-gnome2-0.90.4/ruby-gtk2-0.90.4.tar.gz
-
-inst http://rubygems.org/downloads/nokogiri-1.4.3.1.gem
-
-# inst cutter-1.1.5.tar.gz
-inst http://groonga.org/files/groonga/groonga-1.0.2.tar.gz
-inst http://rubygems.org/downloads/rroonga-1.0.1.gem
-inst http://rubygems.org/downloads/racknga-0.9.0.gem
-inst http://rubyforge.org/frs/download.php/73173/chupatext-0.4.0.tar.gz
-# inst http://rubygems.org/downloads/chuparuby-0.5.0.gem
-inst http://rubygems.org/downloads/chupatext-0.4.0.gem
-# inst ranguba-0.1.0.tar.gz
-
-download http://rubygems.org/downloads/abstract-1.0.0.gem
-download http://rubygems.org/downloads/actionmailer-3.0.1.gem
-download http://rubygems.org/downloads/actionpack-3.0.1.gem
-download http://rubygems.org/downloads/activemodel-3.0.1.gem
-download http://rubygems.org/downloads/activerecord-3.0.1.gem
-download http://rubygems.org/downloads/activeresource-3.0.1.gem
-download http://rubygems.org/downloads/activesupport-3.0.1.gem
-download http://rubygems.org/downloads/arel-1.0.1.gem
-download http://rubygems.org/downloads/builder-2.1.2.gem
-download http://rubygems.org/downloads/bundler-1.0.5.gem
-download http://rubygems.org/downloads/erubis-2.6.6.gem
-download http://rubygems.org/downloads/i18n-0.4.1.gem
-download http://rubygems.org/downloads/mail-2.2.7.gem
-download http://rubygems.org/downloads/mime-types-1.16.gem
-download http://rubygems.org/downloads/polyglot-0.3.1.gem
-download http://rubygems.org/downloads/rack-1.2.1.gem
-download http://rubygems.org/downloads/rack-mount-0.6.13.gem
-download http://rubygems.org/downloads/rack-test-0.5.6.gem
-download http://rubygems.org/downloads/railties-3.0.1.gem
-download http://rubygems.org/downloads/thor-0.14.3.gem
-download http://rubygems.org/downloads/treetop-1.4.8.gem
-download http://rubygems.org/downloads/tzinfo-0.3.23.gem
-inst http://rubygems.org/downloads/rails-3.0.1.gem
-download http://rubygems.org/downloads/fastthread-1.0.7.gem
-download http://rubygems.org/downloads/daemon_controller-0.2.5.gem
-download http://rubygems.org/downloads/spruz-0.2.2.gem
-download http://rubygems.org/downloads/file-tail-1.0.5.gem
-inst http://rubygems.org/downloads/passenger-3.0.0.gem
-
-if test "$showlist" = yes; then
-    echo "Packages to be installed:"
-    show=yes
-    for inst in "${installs[@]}"; do
-	if test "$inst" = "$SEPARATOR"; then
-	    show=yes
-	else
-	    test "$show" = yes && echo "* $inst"
-	    show=no
-	fi
-    done
-    echo "Packages to be downloaded:"
-    for url in "${missing[@]}"; do
-	echo "* $url"
-    done
-else
-    download_all
-    test "$noinst" = yes || install_all
-fi
+sudo -H -u $RANGUBA_USERNAME \
+    nocheck="$nocheck" \
+    noinst="$noinst" \
+    showlist="$showlist" \
+    PREFIX="$PREFIX" \
+    SOURCE="$SOURCE" \
+    SEPARATOR="'$SEPARATOR'" \
+    bash ./install_from_source_packages.sh
 
 test $fd && echo "Finished: $(LC_ALL=C date)" 1>&$log
 exec 3>&-
+
+# Local Variables:
+# tab-width: 8
+# indent-tabs-mode: t
+# End:
+
---
+++ installer/install_from_source_packages.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+set -e
+
+function abort() {
+    local status=$?
+    if test $# = 0; then
+	echo Failed
+    else
+	echo "$*"
+    fi
+    if test $status = 0; then
+	status=1
+    fi
+    exit $status
+}
+
+function download() {
+    for url do
+	test -f "${SOURCE}/${url##*/}" || missing=("${missing[@]}" "$url")
+    done
+}
+
+function inst() {
+    local base="${1##*/}"
+    download "$1"
+    shift
+    installs=("${installs[@]}" "$SEPARATOR" "$base" "$@")
+}
+
+function do_install1() {
+    local base patched
+
+    echo "Install: [$(date +%Y/%m/%d-%H:%M:%S)] $1" 1>&$log
+
+    case "$1" in
+      (*.tar.bz2)
+	echo -n "Extracting $1..."
+	mkdir -p build
+	tar xpjf "${SOURCE}/$1" -C build 1>&$log 2>&1 || abort
+	echo done
+	base=${1%.tar.bz2}
+	;;
+      (*.tar.gz)
+	echo -n "Extracting $1..."
+	mkdir -p build
+	tar xpzf "${SOURCE}/$1" -C build 1>&$log 2>&1 || abort
+	echo done
+	base=${1%.tar.gz}
+	;;
+      (*.gem)
+	echo -n "Installing $1..."
+	ruby -C "${SOURCE}" -S gem install --no-ri --no-rdoc --local "$1" 1>&$log 2>&1 || abort
+	echo done
+	return
+	;;
+    esac
+
+    while shift; do
+	case "$1" in
+	  (--patch=*)
+	    echo -n "Applying patch ${1#*=} to $base..."
+	    patch -d "build/$base" -p1 < "${SOURCE}/${1#*=}" 1>&$log 2>&1 || abort
+	    patched=yes
+	    echo done
+	    ;;
+	  (*)
+	    break
+	    ;;
+	esac
+    done
+
+    (cd "build/$base"
+    if test \( -f configure.in -a ! configure -nt configure.in \) \
+	 -o \( -f configure.ac -a ! configure -nt configure.ac \); then
+	exec autoconf
+    fi) 1>&$log 2>&1
+
+    if test -f "build/$base/configure"; then
+	echo -n "Configuring $base..."
+	(
+	    cd "build/$base"
+	    exec ./configure --enable-shared --prefix="$PREFIX" "$@"
+	) 1>&$log 2>&1 || abort
+	echo done
+    fi
+
+    echo -n "Building $base..."
+    if test -f "build/$base/Makefile"; then
+	test "$patched" = yes && make -C "build/$base" prereq 1>&$log 2>&1 || true
+	make -C "build/$base" 1>&$log 2>&1 || abort
+    elif test -f "build/$base/Rakefile"; then
+	ruby -C "build/$base" -S rake 1>&$log 2>&1 || abort
+    fi
+    echo done
+
+    echo -n "Installing $base..."
+    if test -f "build/$base/GNUmakefile" -o -f "build/$base/Makefile"; then
+	make -C "build/$base" prefix="$PREFIX" install 1>&$log 2>&1 || abort
+    elif test -f "build/$base/Rakefile"; then
+	ruby -C "build/$base" -S rake install 1>&$log 2>&1 || abort
+    fi
+    echo done
+
+    rm -fr "build/$base" > /dev/null 2>&1 || :
+    rmdir build >/dev/null 2>&1 || :
+}
+
+function install_passenger() {
+    # 設定ファイルは適切な位置にコピーする
+    ruby -S passenger-install-apache2-module -a || abort
+    if [ ! -f passenger.conf ]; then
+        ruby -S passenger-install-apache2-module --snippet > passenger.conf
+    fi
+    if [ ! -f ranguba.conf ]; then
+	# virtualhost はやめる
+	cat > ranguba.conf <<EOF
+<VirtualHost *:80>
+   ServerName www.example.com
+   DocumentRoot ${PREFIX}/srv/www/ranguba/public
+   <Directory ${PREFIX}/srv/www/ranguba/public>
+      AllowOverride all
+      Options -MultiViews
+   </Directory>
+</VirtualHost>
+EOF
+    fi
+}
+
+function download_all() {
+    if test ${#missing[@]} -gt 0; then
+	if test ! -z "${missing}"; then
+	    wget -N -P "${SOURCE}" "${missing[@]}" || abort
+	fi
+    fi
+}
+
+function install_all() {
+    local args arg
+    until test ${#installs[@]} = 0; do
+	args=()
+	until test ${#installs[@]} = 0 || {
+		arg="${installs}" installs=("${installs[@]:1}")
+		test "$arg" = "$SEPARATOR"
+	    } do
+	    args=("${args[@]}" "${arg}")
+	done
+	test ${#args[@]} -gt 0 && do_install1 "${args[@]}"
+    done
+    install_passenger
+}
+
+source ./sourcelist
+
+export PATH="$PREFIX/bin:$PATH"
+export LD_RUN_PATH="$PREFIX/lib:${LD_RUN_PATH-/usr/$lib}"
+export LD_LIBRARY_PATH="$PREFIX/lib:${LD_LIBRARY_PATH-/usr/$lib}"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:${PKG_CONFIG_PATH-/usr/lib/pkgconfig}"
+
+if test "$noinst" = no; then
+    exec 3> install.log
+    log=3
+else
+    log=
+fi
+# exec install
+if test "$showlist" = yes; then
+    echo "Packages to be installed:"
+    show=yes
+    for inst in "${installs[@]}"; do
+	if test "$inst" = "$SEPARATOR"; then
+	    show=yes
+	else
+	    test "$show" = yes && echo "* $inst"
+	    show=no
+	fi
+    done
+    echo "Packages to be downloaded:"
+    for url in "${missing[@]}"; do
+	echo "* $url"
+    done
+else
+    download_all
+    test "$noinst" = yes || install_all
+fi
+
+# Local Variables:
+# tab-width: 8
+# indent-tabs-mode: t
+# End:
+
