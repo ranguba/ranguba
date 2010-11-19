431d5713ed996d01f8fdb5ce52df9a51be7efb61
Kenji Okimoto <okimoto@clear-code.com>
2010-11-19 15:45:50 +0900

    remove public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Ranguba::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "search#index"
 
   # See how all your routes lay out with "rake routes"
 
