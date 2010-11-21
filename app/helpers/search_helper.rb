6de914cabb581d63b966d82b1c6010f4528bbde8
Kouhei Sutou <kou@clear-code.com>
2010-11-21 19:40:10 +0900

    cleanup.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,14 +2,14 @@ module SearchHelper
   def drilldown_link(entry)
     case entry.key
     when :type
-      file_type_icon(entry)
+      file_type_drilldown_link(entry)
     else
       link_to_unless(@search_request.have_key?(entry.key),
                      entry.label, entry.path)
     end
   end
 
-  def file_type_icon(entry)
+  def file_type_drilldown_link(entry)
     link_to_unless(@search_request.have_key?(entry.key),
                    image_tag("file_types/#{entry.value}.png",
                              :alt => entry.label,
