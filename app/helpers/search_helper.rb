f63212ecdfa4535d679db832ef41a1a96075850a
Kouhei Sutou <kou@clear-code.com>
2010-11-21 19:39:28 +0900

    filetype -> file_type.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,16 +2,16 @@ module SearchHelper
   def drilldown_link(entry)
     case entry.key
     when :type
-      filetype_icon(entry)
+      file_type_icon(entry)
     else
       link_to_unless(@search_request.have_key?(entry.key),
                      entry.label, entry.path)
     end
   end
 
-  def filetype_icon(entry)
+  def file_type_icon(entry)
     link_to_unless(@search_request.have_key?(entry.key),
-                   image_tag("filetypes/#{entry.value}.png",
+                   image_tag("file_types/#{entry.value}.png",
                              :alt => entry.label,
                              :size => "24x24"),
                    entry.path)
