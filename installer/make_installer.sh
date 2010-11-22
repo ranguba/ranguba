600e92294cca6eb38520b662a0d0f71b50e4f932
Kenji Okimoto <okimoto@clear-code.com>
2010-11-22 09:46:37 +0900

    use bundler

--- installer/make_installer.sh
+++ installer/make_installer.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
 export BASE_DIR=`pwd`
-export SOURCE=$BASE_DIR/ranguba_installer/sources
+export INSTALLER_DIR=$BASE_DIR/ranguba_installer
+export SOURCE=$INSTALLER_DIR/sources
 
 export nocheck=yes
 export noinst=yes
 
-echo "Creating installer..."
-(cd ../../rroonga/
-    rake gem
-    cp pkg/rroonga*.gem "$SOURCE/"
-)
-(cd ../../activegroonga/
-    rake gem
-    cp pkg/activegroonga*.gem "$SOURCE/"
-)
-./install_sources_and_gems.sh
+function prepare_gems() {
+    echo -n "Prepage gems..."
+    (cd ../../rroonga/
+        rake gem
+        cp pkg/rroonga*.gem "$SOURCE/"
+    )
+    (cd ../../activegroonga/
+        rake gem
+        cp pkg/activegroonga*.gem "$SOURCE/"
+    )
+    (cd ../
+        bundle package
+        cp vendor/cache/*.gem "$SOURCE/"
+    )
+    echo done
+}
 
 mkdir -p $SOURCE
-cp ./{install.sh,install_sources_and_gems.sh,sourcelist} ${BASE_DIR}/ranguba_installer
+prepare_gems
+./install_sources_and_gems.sh
+
+cp ./{install.sh,install_sources_and_gems.sh,sourcelist} "$INSTALLER_DIR/"
+mkdir -p "$INSTALLER_DIR/data"
+cp ./data/* "$INSTALLER_DIR/data/"
 
 (cd ../
     git archive --format=tar --prefix=ranguba/ HEAD | gzip > ./ranguba.tar.gz
     mv ./ranguba.tar.gz "$SOURCE/"
 )
 tar cfz ranguba_installer.tar.gz ranguba_installer
-echo done
