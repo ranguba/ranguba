18caddc2408b881d32a41d36835d981edd5d9389
Kenji Okimoto <okimoto@clear-code.com>
2010-11-24 08:39:43 +0900

    do not use fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
   def run_shell_command(*args)
