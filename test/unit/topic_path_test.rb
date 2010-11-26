24ab8b6df11b595a2880fca85b6c94613d4ad42c
Kenji Okimoto <okimoto@clear-code.com>
2010-11-26 16:45:43 +0900

    fix coding style

--- test/unit/topic_path_test.rb
+++ test/unit/topic_path_test.rb
@@ -13,19 +13,19 @@ class TopicPathTest < ActiveSupport::TestCase
     items << TopicPathItem.new(:query, 'q2')
     items << TopicPathItem.new(:query, 'q3')
     topic_path = TopicPath.new(*items)
-    assert_equal 'type/html/category/blog/query/q1+q2+q3', topic_path.search_request
+    assert_equal('type/html/category/blog/query/q1+q2+q3', topic_path.search_request)
     topic_path = topic_path[0..3]
-    assert_equal 'type/html/category/blog/query/q1+q2', topic_path.search_request
+    assert_equal('type/html/category/blog/query/q1+q2', topic_path.search_request)
     topic_path = topic_path[0..2]
-    assert_equal 'type/html/category/blog/query/q1', topic_path.search_request
+    assert_equal('type/html/category/blog/query/q1', topic_path.search_request)
     topic_path = topic_path[0..1]
-    assert_equal 'type/html/category/blog', topic_path.search_request
+    assert_equal('type/html/category/blog', topic_path.search_request)
     topic_path = topic_path[0..0]
-    assert_equal 'type/html', topic_path.search_request
+    assert_equal('type/html', topic_path.search_request)
   end
 
   def test_search_request__empty_items
     topic_path = TopicPath.new
-    assert_nil topic_path.search_request
+    assert_nil(topic_path.search_request)
   end
 end
