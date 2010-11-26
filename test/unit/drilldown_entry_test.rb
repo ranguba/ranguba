49e292b5cf89dac42baf0528c6e502ad664942c1
Kenji Okimoto <okimoto@clear-code.com>
2010-11-26 15:45:09 +0900

    fix test case

--- test/unit/drilldown_entry_test.rb
+++ test/unit/drilldown_entry_test.rb
@@ -1,100 +1,43 @@
 # coding: utf-8
 require 'test_helper'
 
-class DrilldownItemTest < ActiveSupport::TestCase
+class DrilldownEntryTest < ActiveSupport::TestCase
 
   def setup
-    @item = DrilldownItem.new
+    @item = DrilldownEntry.new
   end
 
   def test_new
-    assert_valid
+    assert !@item.query_item?
   end
 
   def test_new_with_params
-    @item = DrilldownItem.new({})
-    assert_valid
-    
-    @item = DrilldownItem.new(:base_params => "query/q",
-                              :param => :type,
-                              :value => "t")
-    assert_valid(:to_s => "query/q/type/t",
-                 :query => "q",
-                 :type => "t",
-                 :param => :type,
-                 :value => "t",
-                 :empty => false)
+    @item = DrilldownEntry.new({})
+    assert_properties
 
-    @item = DrilldownItem.new(:base_params => "category/c",
-                              :param => :query,
-                              :value => "q")
-    assert_valid(:to_s => "category/c/query/q",
-                 :canonical => "query/q/category/c",
-                 :category => "c",
-                 :query => "q",
-                 :param => :query,
-                 :value => "q",
-                 :empty => false)
-  end
+    @item = DrilldownEntry.new(:key => :type, :value => "t")
+    assert_properties(:key => :type, :value => "t")
 
-  def test_to_hash
-    @item = DrilldownItem.new(:base_params => "type/t")
-    @item.param = :category
-    @item.value = "c"
-    assert_equal({:type => "t", :category => "c"}, @item.to_hash)
-    assert_equal({:category => "c", :type => "t"}, @item.to_hash(:canonical => true))
-    assert_equal({:type => "t", :category => "c"}, @item.to_hash(:query => "foo"))
-  end
-
-  def test_to_s
-    @item = DrilldownItem.new(:base_params => "type/t")
-    @item.param = :category
-    @item.value = "c"
-    assert_equal "type/t/category/c", @item.to_s
-    assert_equal "category/c/type/t", @item.to_s(:canonical => true)
-    assert_equal "type/t/category/c", @item.to_s(:query => "q")
+    @item = DrilldownEntry.new(:key => :query, :value => "q")
+    assert_properties(:key => :query, :value => "q")
   end
 
   def test_path
-    @item = DrilldownItem.new(:base_params => "type/t")
-    @item.param = :category
+    @item = DrilldownEntry.new
+    @item.key = :category
     @item.value = "c"
-    assert_equal "/base/type/t/category/c", @item.path(:base_path => "/base/")
-    assert_equal "/base/category/c/type/t", @item.path(:base_path => "/base/", :canonical => true)
-    assert_equal "/base/type/t/category/c", @item.path(:base_path => "/base/", :query => "q")
+    assert_equal "category/c", @item.path
   end
 
   private
-  def assert_valid(options={})
-    assert @item.valid?
-    assert_properties(options)
-  end
-
-  def assert_invalid(options={})
-    assert_false @item.valid?
-    assert_properties(options)
-  end
 
   def assert_properties(options={})
-    options[:to_s] ||= ""
-    options[:canonical] ||= options[:to_s]
-    options[:query] ||= nil
-    options[:category] ||= nil
-    options[:type] ||= nil
-    options[:param] ||= nil
+    options[:key] ||= nil
     options[:value] ||= nil
     options[:count] ||= 0
-    options[:empty] = true if options[:empty].nil?
-
-    assert_equal options[:to_s], @item.to_s
-    assert_equal options[:canonical], @item.to_s(:canonical => true)
-    assert_equal options[:query], @item.query
-    assert_equal options[:category], @item.category
-    assert_equal options[:type], @item.type
-    assert_equal options[:param], @item.param
+    assert_equal options[:key], @item.key
     assert_equal options[:value], @item.value
     assert_equal options[:count], @item.count
-    assert_equal options[:empty], @item.empty?
   end
 
 end
