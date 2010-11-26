a61e3cc4c039d1017372265c45f54d2aa7562b35
Kenji Okimoto <okimoto@clear-code.com>
2010-11-26 14:34:39 +0900

    add TopicPath class

---
+++ app/models/topic_path.rb
@@ -0,0 +1,58 @@
+require 'forwardable'
+
+class TopicPath
+  extend Forwardable
+  include Enumerable
+
+  def_delegators :@items, :first, :last, :empty?
+
+  def initialize(*items)
+    @items = items
+  end
+
+  def delete_item(item)
+    new_items = @items.dup
+    new_items.delete(item)
+    TopicPath.new(*new_items)
+  end
+
+  def delete_item_at(index)
+    new_items = @items.dup
+    new_items.delete_at(index)
+    TopicPath.new(*new_items)
+  end
+
+  def [](*args)
+    new_items = @items[*args]
+    TopicPath.new(*new_items)
+  end
+
+  def title
+    @items.map(&:label).join(I18n.t("search_conditions_delimiter"))
+  end
+
+  def search_request
+    @items.inject([]){|memo, item|
+      if item.query_item?
+        memo += [query] unless memo.any?{|v| /query/ =~ v }
+      else
+        memo += [item.condition]
+      end
+      memo
+    }.join('/')
+  end
+
+  def query
+    first, *rest = @items.select{|item| item.query_item? }
+    rest_terms = rest.map(&:value).join('+')
+    rest_terms = "+#{rest_terms}" unless rest_terms.blank?
+    "#{first.condition}#{rest_terms}"
+  end
+
+  def each
+    @items.each do |item|
+      yield item
+    end
+  end
+
+end
---
+++ test/unit/topic_path_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class TopicPathTest < ActiveSupport::TestCase
+
+  def setup
+  end
+
+  def test_search_request
+    items = []
+    items << TopicPathItem.new(:type, 'html')
+    items << TopicPathItem.new(:category, 'blog')
+    items << TopicPathItem.new(:query, 'q1')
+    items << TopicPathItem.new(:query, 'q2')
+    items << TopicPathItem.new(:query, 'q3')
+    topic_path = TopicPath.new(*items)
+    assert_equal 'type/html/category/blog/query/q1+q2+q3', topic_path.search_request
+    topic_path = topic_path[0..3]
+    assert_equal 'type/html/category/blog/query/q1+q2', topic_path.search_request
+    topic_path = topic_path[0..2]
+    assert_equal 'type/html/category/blog/query/q1', topic_path.search_request
+    topic_path = topic_path[0..1]
+    assert_equal 'type/html/category/blog', topic_path.search_request
+    topic_path = topic_path[0..0]
+    assert_equal 'type/html', topic_path.search_request
+  end
+end
