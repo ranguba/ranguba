45fe5b352e6839609dad00892760d88d9b6c7130
Kenji Okimoto <okimoto@clear-code.com>
2010-12-17 18:18:43 +0900

    refactor test code

--- test/unit/ranguba/indexer_test.rb
+++ test/unit/ranguba/indexer_test.rb
@@ -5,48 +5,57 @@ require 'ranguba/indexer'
 class Ranguba::IndexerTest < ActiveSupport::TestCase
 
   def setup
-    @klass = Ranguba::Indexer
-    @basic = @klass.new([])
+    @indexer = Ranguba::Indexer.new([])
   end
 
   def test_valid_utf8_with_empty_string
-    assert @basic.send(:valid_utf8?, "")
+    assert_valid_utf8("")
   end
 
   def test_valid_utf8_with_ascii_only_string
-    assert @basic.send(:valid_utf8?, "abcdefg123")
+    assert_valid_utf8("abcdefg123")
   end
 
   def test_valid_utf8_with_white_space_string
-    assert @basic.send(:valid_utf8?, " \t\n")
+    assert_valid_utf8(" \t\n")
   end
 
   def test_valid_utf8_with_utf8_string__hiragana
-    assert @basic.send(:valid_utf8?, "あいうえお")
+    assert_valid_utf8("あいうえお")
   end
 
   def test_valid_utf8_with_false_positive_string
     # "フォーマット集".force_encoding('sjis').valid_encoding? # => true
-    assert @basic.send(:valid_utf8?, "フォーマット集")
+    assert_valid_utf8("フォーマット集")
   end
 
   def test_valid_utf8_with_utf8_string__kanji
-    assert @basic.send(:valid_utf8?, "日本語の漢字を表現します")
+    assert_valid_utf8("日本語の漢字を表現します")
   end
 
   def test_valid_utf8_with_sjis_string__hiragana
-    assert !@basic.send(:valid_utf8?, "あいうえお".encode('Shift_JIS'))
+    assert_not_valid_utf8("あいうえお".encode('Shift_JIS'))
   end
 
   def test_valid_utf8_with_sjis_string__kanji
-    assert !@basic.send(:valid_utf8?, "日本語の漢字を表現します".encode('Shift_JIS'))
+    assert_not_valid_utf8("日本語の漢字を表現します".encode('Shift_JIS'))
   end
 
   def test_valid_utf8_with_eucjp_string__hiragana
-    assert !@basic.send(:valid_utf8?, "あいうえお".encode('EUC-JP'))
+    assert_not_valid_utf8("あいうえお".encode('EUC-JP'))
   end
 
   def test_valid_utf8_with_eucjp_string__kanji
-    assert !@basic.send(:valid_utf8?, "日本語の漢字を表現します".encode('EUC-JP'))
+    assert_not_valid_utf8("日本語の漢字を表現します".encode('EUC-JP'))
+  end
+
+  private
+
+  def assert_valid_utf8(string)
+    assert_true(@indexer.send(:valid_utf8?, string))
+  end
+
+  def assert_not_valid_utf8(string)
+    assert_false(@indexer.send(:valid_utf8?, string))
   end
 end
