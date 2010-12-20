976fdf282f4fd614eb3b890780c101e5634b071f
Kenji Okimoto <okimoto@clear-code.com>
2010-12-20 15:09:37 +0900

    fix test case

--- test/unit/ranguba/encoding_loader_test.rb
+++ test/unit/ranguba/encoding_loader_test.rb
@@ -8,7 +8,7 @@ class Ranguba::EncodingLoaderTest < ActiveSupport::TestCase
 
   def test_load
     hash = @loader.load
-    assert_equal(hash['categories.csv'], Encoding.find('Shift_JIS'))
+    assert_equal(hash['categories.csv'], Encoding.find('UTF-8'))
     assert_equal(hash['title.txt'], Encoding.find('UTF-8'))
     assert_equal(hash['header.txt'], Encoding.find('EUC-JP'))
     assert_equal(hash['footer.txt'], Encoding.find('Shift_JIS'))
