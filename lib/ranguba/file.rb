2cafe99b7a6158fdad052ebcd088b5bba8962ae7
Kenji Okimoto <okimoto@clear-code.com>
2010-12-21 09:59:11 +0900

    fix coding style

--- lib/ranguba/file.rb
+++ lib/ranguba/file.rb
@@ -1,11 +1,11 @@
 class Ranguba::File
 
-  def self.read(path, external_encoding = Encoding.find('utf-8'))
+  def self.read(path, external_encoding=Encoding.find('utf-8'))
     return '' unless File.exist?(path)
     if external_encoding == Encoding.find('utf-8')
-      File.open(path, "r:utf-8"){|file| file.read }
+      File.open(path, "r:utf-8") {|file| file.read}
     else
-      File.open(path, "r:#{external_encoding}:utf-8"){|file| file.read }
+      File.open(path, "r:#{external_encoding}:utf-8") {|file| file.read}
     end
   end
 
