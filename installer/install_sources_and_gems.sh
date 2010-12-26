4bb4095adfb34ce611c00e0f88ffb72725ff7328
Kouhei Sutou <kou@clear-code.com>
2010-12-26 09:36:41 +0900

    don't redirect indexer log by shell.

--- installer/install_sources_and_gems.sh
+++ installer/install_sources_and_gems.sh
@@ -210,8 +210,7 @@ function install_crontab() {
 	cp -f "$DATA_DIR/make_index" "$PREFIX/bin/make_index"
     fi
     local COMMAND="${PREFIX}/bin/make_index"
-    local LOG_DIR="${PREFIX}/${APPLICATION_NAME}/log"
-    echo "0 2 * * * $COMMAND > ${LOG_DIR}/indexer.log" | crontab -
+    echo "0 2 * * * $COMMAND" | crontab -
     echo "done"
 }
 
