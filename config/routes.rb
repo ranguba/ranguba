6a1e7c93161bbd6072623a28a64d05b06a2536a1
Kouhei Sutou <kou@clear-code.com>
2012-01-02 14:28:23 +0900

    re-add removed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Ranguba::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match "search/(*search_request)", :to => "ranguba/search#index", :as => 'search'
+  match "help", :to => "ranguba/help#index", :as => "help"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
