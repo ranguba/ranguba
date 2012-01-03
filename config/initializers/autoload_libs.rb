b528cbd43ee772c14cb94f555e143e6d9097e648
Kouhei Sutou <kou@clear-code.com>
2012-01-03 23:52:07 +0900

    move topic_path.rb to lib/ from app/models/.

--- config/initializers/autoload_libs.rb
+++ config/initializers/autoload_libs.rb
@@ -4,6 +4,7 @@ require 'ranguba/category_loader'
 require 'ranguba/type_loader'
 require 'ranguba/password_loader'
 require 'ranguba/template'
+require 'ranguba/topic_path'
 
 Rails.configuration.ranguba_config_encodings = Ranguba::EncodingLoader.new.load
 
