367c4a47b9f307c52fab97587ce3e553a9a311e2
Sutou Kouhei <kou@clear-code.com>
2021-03-15 12:35:49 +0900

    test: use headless_chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium,
+            using: :headless_chrome,
+            screen_size: [1400, 1400] do |driver_option|
+    driver_option.add_argument("--no-sandbox")
+  end
 end
