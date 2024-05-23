1f443eb5fec47445c8ddc60dfa4009c91f65f58c
otegami <otegami@clear-code.com>
2024-05-23 18:13:20 +0900

    bin/rails app:update

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-if (RUBY_VERSION <=> "3.1.0") < 0
-  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-end
