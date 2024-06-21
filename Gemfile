0c8277d09a74680665de93de280f22e9e95c0d9f
takuya kodama <otegami@clear-code.com>
2024-06-21 09:00:40 +0800

    Update Rails to v7.0.0 (#9)
    ref: https://github.com/ranguba/ranguba/issues/7
    
    This change upgrade Rails version from `6.1.3` to `7.0.0`.
    We have adopted the default Rails settings for the updated changes to
    ensure maintainability.
    
    ## For reviewers.
    
    Except for the following change, every change is auto generated by
    `bin/rails app:update` command.
    - https://github.com/ranguba/ranguba/pull/9/files#r1648285559
    
    ## What I will do at the following PRs
    - [ ] Remove `gem listen`
    - [ ] Configure new Rails default settings one by one

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 6.1.3"
+gem "rails", "~> 7.0.0"
 # Use Puma as the app server
 gem "puma", "~> 5.0"
 # Use SCSS for stylesheets
--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-if (RUBY_VERSION <=> "3.1.0") < 0
-  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-end
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
