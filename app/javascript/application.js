b6ca3512a56e0798f809b6c859c6978c61434685
otegami <a.s.takuya1026@gmail.com>
2024-07-29 17:00:46 +0800

    bin/importmap unpin @rails/ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs"
 import "channels"
-
-Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,5 @@
 
 pin "application"
 pin "@rails/actioncable", to: "actioncable.esm.js"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.0.8
 
 pin_all_from "app/javascript/channels", under: "channels"
